830fd453e7607faa5686253e4d452aa841a9463c
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-11-23 09:47:59 -0500

    error fixes in seeds file

--- db/database.js
+++ db/database.js
@@ -1,12 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
 
 //connect to database
-const db = new sqlite3.Database('./db/election.db', err => {
+const db = new sqlite3.Database('./db/tracker.db', err => {
     if (err) {
         return console.error(err.message);
      }
 
-     console.log('Connected to the election database.');
+     console.log('Connected to the employee tracker database.');
 });
 
 module.exports = db;
