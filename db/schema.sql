2ccaeb0a5c8dc915abebc72434b9d380241a9486
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-11-23 09:04:51 -0500

    update schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,23 @@
 DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
     id INTEGER PRIMARY KEY,
     name VARCHAR(30) NOT NULL
+    );
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
+    );
+
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
     );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,24 @@ VALUES
     ('engineering'),
     ('finance'), 
     ('legal');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Sales Lead', 45000, 1),
+    ('Sales Person', 40000, 1),
+    ('Lead Engineer', 65000, 2),
+    ('Software Engineer', 60000, 2),
+    ('Accountant', 60000, 3),
+    ('Legal Team Lead', 75000, 4),
+    ('Laywer', 70000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Malia', 'Brown', 4, 3),
+    ('Sarah', 'Lourd', 5),
+    ('Tom', 'Allen', 6),
+    ('Tammer', 'Galal', 7, 6);
